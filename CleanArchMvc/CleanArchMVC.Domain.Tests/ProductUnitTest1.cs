b132782b33aef68569f1eab05c30b86abe2a63a6
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-11-04 09:26:03 -0300

    Implementado testes unitários de produtos

---
+++ CleanArchMvc/CleanArchMVC.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Validation;
+using FluentAssertions;
+
+namespace CleanArchMVC.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "product image");
+
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "product image");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid id value");
+        }
+
+        [Fact]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "product image");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Product Teste", "Product Description", 9.99m, 99, "productfsdfsdfsdfsdfsdafwefwefwefwefwefwefwefwefwefwefwefew imageooooooooooooooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaoooooooooooooooo toooooooooooooooooooooooooooooooooooooo looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid image name,too long, maximum 250 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Teste", "Product Description", 9.99m, 99, null);
+
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Teste", "Product Description", 9.99m, 99, "Image teste");
+
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Teste", "Product Description", -9.99m, 99, "Image teste");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid price value");
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value)
+        {
+            Action action = () => new Product(1, "Product Teste", "Product Description", 9.99m, value, "Image teste");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid stock value");
+        }
+
+
+    }
+}
--- CleanArchMvc/CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Product.cs
@@ -41,7 +41,7 @@ namespace CleanArchMvc.Domain.Entities
             DomainExceptionValidation.When(description.Length < 3, "Invalid description, too short, minimum 3 characters");
 
             DomainExceptionValidation.When(price < 0, "Invalid price value");
-            DomainExceptionValidation.When(stock < 0, "Invalid price value");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
 
             DomainExceptionValidation.When(image.Length > 250, "Invalid image name,too long, maximum 250 characters");
 
